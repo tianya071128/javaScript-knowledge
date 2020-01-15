0734039fe8d43538d43a776b506c478cc1ad323e
温祖彪 <wenzubiao@126.com>
2020-01-15 18:08:43 +0800

    es6 - 模块

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion: 
+ * @Author: sueRimn
+ * @Date: 2020-01-15 16:44:54
+ * @LastEditTime : 2020-01-15 16:56:04
+ */
+console.log('执行一次');
+
+// DOM 并没有准备就绪
+console.log(document.getElementById('box')) // null
+
+// export default 2;
