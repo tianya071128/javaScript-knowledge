9abf37733bce52eff8bad69a7180ac1c10619241
wenzubiao <1033051985@qq.com>
2021-04-16 00:13:24 +0800

    node15

--- node/bilibili_nodejs_zuixinjiaocheng/06-lagou-admin/serve/app.js
+++ node/bilibili_nodejs_zuixinjiaocheng/06-lagou-admin/serve/app.js
@@ -13,25 +13,29 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/**
+ * 对于 app.use() 和 app[METHOD] 的路径匹配, 一定要看下 API 文档有所介绍
+ */
+
 app.use(logger('dev'));
 
 // 它使用 body-parser 解析带有JSON负载的传入请求 
 // 在 v.4.16.0 版本中, 内置了 body-parser 中间件来解析 json 数据
-app.use(express.json());
+app.use(express.json()); // 请求体为 json 的解析
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser()); //cookie
 // 静态文件夹
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 路由
 app.use('/api/users', userRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 404 处理
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler 错误处理
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
