9946828990a50a7aba5af6e715894796a5173152
tianyan071128 <1033051985@qq.com>
2021-09-30 14:58:45 +0800

    浏览器_文件操作

--- node/koa_demo/src/app.js
+++ node/koa_demo/src/app.js
@@ -3,15 +3,15 @@
  * @Author: 温祖彪
  * @Date: 2021-09-08 10:24:36
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-09-12 12:06:27
+ * @LastEditTime: 2021-09-29 15:29:56
  */
-const Koa = require('koa');
+const Koa = require("koa");
 
-const compose = require('koa-compose'); // 方便中间件管理
-const MD = require('./middlewares/index');
+const compose = require("koa-compose"); // 方便中间件管理
+const MD = require("./middlewares/index");
 
-const config = require('../config'); // 加载配置文件
-const utils = require('./common/utils');
+const config = require("../config"); // 加载配置文件
+const utils = require("./common/utils");
 const app = new Koa(); // 创建一个 app
 
 app.context.utils = utils;
@@ -19,18 +19,30 @@ app.context.utils = utils;
 // 初始化中间件
 app.use(compose(MD));
 
-
 // 监听全局错误
-app.on('error', (err, ctx) => {
+app.on("error", (err, ctx) => {
   if (ctx) {
     ctx.body = {
       code: 9999,
-      message: `程序运行时报错：${err.message}`
+      message: `程序运行时报错：${err.message}`,
     };
   }
 });
 
 // 监听端口
 app.listen(config.port, () => {
-  console.log('服务启动成功');
+  console.log("服务启动成功");
 });
+
+
+
+if (ctrl.setSelectionRange) {
+  ctrl.focus();
+  ctrl.setSelectionRange(pos, pos);
+} else if (ctrl.createTextRange) {
+  var range = ctrl.createTextRange();
+  range.collapse(true);
+  range.moveEnd("character", pos);
+  range.moveStart("character", pos);
+  range.select();
+}
