d864904a0b8e9746c573b686578d871e0e1dd1a2
wenzubiao <1033051985@qq.com>
2021-12-04 21:50:16 +0800

    react12

--- 库、框架/react/02-b站教程/react_demo/src router/index.js
+++ 库、框架/react/02-b站教程/react_demo/src router/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   // <React.StrictMode> 组件用于检查 <APP> 组件 react 代码是否规范
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
