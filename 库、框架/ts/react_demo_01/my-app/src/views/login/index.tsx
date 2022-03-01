271ae2e4385642ae2899278549c2e5c5f498321f
wenzubiao <1033051985@qq.com>
2022-03-01 23:40:42 +0800

    react-ts-05

--- 库、框架/ts/react_demo_01/my-app/src/views/login/index.tsx
+++ 库、框架/ts/react_demo_01/my-app/src/views/login/index.tsx
@@ -1,4 +1,5 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 import './index.scss';
 
@@ -13,44 +14,39 @@ export default function Login(props: any) {
 
   return (
     <div className='_login_'>
+      <h3 className='login_title'>系统登录</h3>
       <Form
         /** 表单名称，会作为表单字段 id 前缀使用	 */
-        name='loginForm'
+        name='login_form'
+        size='large'
         /** label 标签布局 */
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
+        // labelCol={{ span: 8 }}
+        /** 需要为输入控件设置布局样式时，使用该属性，用法同 labelCol	 */
+        // wrapperCol={{ span: 16 }}
         /** 表单默认值，只有初始化以及重置时生效	 */
-        initialValues={{ remember: true }}
+        // initialValues={{ remember: true }}
         /** 提交表单且数据验证成功后回调事件	 */
         onFinish={onFinish}
         /** 提交表单且数据验证失败后回调事件	 */
         onFinishFailed={onFinishFailed}
+        /** 原生 autocomplete 属性，禁用自动完成功能 */
         autoComplete='off'
         className='login_form'>
         <Form.Item
-          label='Username'
           name='username'
           rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input />
+          <Input placeholder='账号' prefix={<UserOutlined />} />
         </Form.Item>
 
         <Form.Item
-          label='Password'
           name='password'
           rules={[{ required: true, message: 'Please input your password!' }]}>
-          <Input.Password />
+          <Input.Password placeholder='密码' prefix={<LockOutlined />} />
         </Form.Item>
 
-        <Form.Item
-          name='remember'
-          valuePropName='checked'
-          wrapperCol={{ offset: 8, span: 16 }}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Form.Item>
           <Button type='primary' htmlType='submit'>
-            Submit
+            登录
           </Button>
         </Form.Item>
       </Form>
